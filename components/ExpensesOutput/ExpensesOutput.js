eea9df70053893288a8614d4d97e948e76098909
Uthaeus <romanlavery@gmail.com>
2023-03-18 14:35:49 -0600

    created expenseitem component w/ styling to be used in expenseslist >> expensesoutput

--- components/ExpensesOutput/ExpensesOutput.js
+++ components/ExpensesOutput/ExpensesOutput.js
@@ -34,7 +34,25 @@ const DUMMY_EXPENSES = [
         description: 'Another book',
         amount: 15.89,
         date: new Date('2022-1-14')
-    }
+    },
+    {
+        id: 'e6',
+        description: 'A pair of shoes',
+        amount: 59.99,
+        date: new Date('2021-12-19')
+    },
+    {
+        id: 'e7',
+        description: 'A pair of trousers',
+        amount: 89.29,
+        date: new Date('2022-1-19')
+    },
+    {
+        id: 'e8',
+        description: 'Some bananas',
+        amount: 5.99,
+        date: new Date('2021-12-22')
+    },
 ];
 
 function ExpensesOutput({expenses, expensesPeriod}) {
@@ -51,7 +69,9 @@ export default ExpensesOutput;
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        padding: 24, 
+        paddingHorizontal: 24, 
+        paddingTop: 24,
+        paddingBottom: 0,
         backgroundColor: GlobalStyles.colors.primary700
     }
 });
