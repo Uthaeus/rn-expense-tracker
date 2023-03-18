f07e0a14a96cd18fcc7edca945a96198ee55a22b
Uthaeus <romanlavery@gmail.com>
2023-03-18 12:27:42 -0600

    created an array of dummy data for use in allexpenses and recentexpenses >> expensesoutput

--- components/ExpensesOutput/ExpensesOutput.js
+++ components/ExpensesOutput/ExpensesOutput.js
@@ -3,11 +3,44 @@ import { View } from "react-native";
 import ExpensesList from "./ExpensesList";
 import ExpensesSummary from "./ExpensesSummary";
 
+const DUMMY_EXPENSES = [
+    {
+        id: 'e1',
+        description: 'A pair of shoes',
+        amount: 59.99,
+        date: new Date('2021-12-19')
+    },
+    {
+        id: 'e2',
+        description: 'A pair of trousers',
+        amount: 89.29,
+        date: new Date('2022-1-19')
+    },
+    {
+        id: 'e3',
+        description: 'Some bananas',
+        amount: 5.99,
+        date: new Date('2021-12-22')
+    },
+    {
+        id: 'e4',
+        description: 'A book',
+        amount: 14.99,
+        date: new Date('2022-2-19')
+    },
+    {
+        id: 'e5',
+        description: 'Another book',
+        amount: 15.89,
+        date: new Date('2022-1-14')
+    }
+];
+
 function ExpensesOutput({expenses, expensesPeriod}) {
     return (
         <View>
-            <ExpensesSummary expenses={expenses} periodName={expensesPeriod} />
-            <ExpensesList />
+            <ExpensesSummary expenses={DUMMY_EXPENSES} periodName={expensesPeriod} />
+            <ExpensesList  />
         </View>
     );
 }
