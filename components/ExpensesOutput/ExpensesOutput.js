ce6dca3c5210b264b39af0730e858c67d8eb1193
Uthaeus <romanlavery@gmail.com>
2023-03-18 21:32:47 -0600

    created expenses context file, moved dummy expenses, created context and reducer functions

--- components/ExpensesOutput/ExpensesOutput.js
+++ components/ExpensesOutput/ExpensesOutput.js
@@ -4,56 +4,7 @@ import { GlobalStyles } from "../../constants/styles";
 import ExpensesList from "./ExpensesList";
 import ExpensesSummary from "./ExpensesSummary";
 
-const DUMMY_EXPENSES = [
-    {
-        id: 'e1',
-        description: 'A pair of shoes',
-        amount: 59.99,
-        date: new Date('2021-12-19')
-    },
-    {
-        id: 'e2',
-        description: 'A pair of trousers',
-        amount: 89.29,
-        date: new Date('2022-1-19')
-    },
-    {
-        id: 'e3',
-        description: 'Some bananas',
-        amount: 5.99,
-        date: new Date('2021-12-22')
-    },
-    {
-        id: 'e4',
-        description: 'A book',
-        amount: 14.99,
-        date: new Date('2022-2-19')
-    },
-    {
-        id: 'e5',
-        description: 'Another book',
-        amount: 15.89,
-        date: new Date('2022-1-14')
-    },
-    {
-        id: 'e6',
-        description: 'A pair of shoes',
-        amount: 59.99,
-        date: new Date('2021-12-19')
-    },
-    {
-        id: 'e7',
-        description: 'A pair of trousers',
-        amount: 89.29,
-        date: new Date('2022-1-19')
-    },
-    {
-        id: 'e8',
-        description: 'Some bananas',
-        amount: 5.99,
-        date: new Date('2021-12-22')
-    },
-];
+
 
 function ExpensesOutput({expenses, expensesPeriod}) {
     return (
