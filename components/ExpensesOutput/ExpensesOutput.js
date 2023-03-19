7be1968b9b0f210e6905c1bc31763114d758c4c9
Uthaeus <romanlavery@gmail.com>
2023-03-19 15:03:12 -0600

    created fallback text for recent and all expenses to display when no matching expenses found

--- components/ExpensesOutput/ExpensesOutput.js
+++ components/ExpensesOutput/ExpensesOutput.js
@@ -1,4 +1,4 @@
-import { View, StyleSheet } from "react-native";
+import { View, Text, StyleSheet } from "react-native";
 import { GlobalStyles } from "../../constants/styles";
 
 import ExpensesList from "./ExpensesList";
@@ -6,11 +6,17 @@ import ExpensesSummary from "./ExpensesSummary";
 
 
 
-function ExpensesOutput({expenses, expensesPeriod}) {
+function ExpensesOutput({expenses, expensesPeriod, fallbackText}) {
+    let content = <Text style={styles.infoText}>{fallbackText}</Text>;
+
+    if (expenses.length > 0) {
+        content = <ExpensesList expenses={expenses} />;
+    }
+
     return (
         <View style={styles.container}>
             <ExpensesSummary expenses={expenses} periodName={expensesPeriod} />
-            <ExpensesList expenses={expenses} />
+            {content}
         </View>
     );
 }
@@ -24,5 +30,11 @@ const styles = StyleSheet.create({
         paddingTop: 24,
         paddingBottom: 0,
         backgroundColor: GlobalStyles.colors.primary700
+    },
+    infoText: {
+        color: 'white',
+        fontSize: 16,
+        textAlign: 'center',
+        marginTop: 32
     }
 });
--- screens/AllExpenses.js
+++ screens/AllExpenses.js
@@ -4,9 +4,15 @@ import ExpensesOutput from "../components/ExpensesOutput/ExpensesOutput";
 import { ExpensesContext } from "../store/expenses-contex";
 
 function AllExpenses() {
-    const expensesCtx = useContext(ExpensesContext);
+  const expensesCtx = useContext(ExpensesContext);
 
-    return <ExpensesOutput expenses={expensesCtx.expenses} expensesPeriod='Total' />
+  return (
+    <ExpensesOutput
+      expenses={expensesCtx.expenses}
+      expensesPeriod="Total"
+      fallbackText="No registered expenses found."
+    />
+  );
 }
 
 export default AllExpenses;
