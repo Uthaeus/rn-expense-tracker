05cb3fc36b4791c3c3d991cade4bf96d0dd12f55
Uthaeus <romanlavery@gmail.com>
2023-03-18 19:22:02 -0600

    setting up structure of new and edit manageexpense

--- screens/ManageExpense.js
+++ screens/ManageExpense.js
@@ -1,19 +1,51 @@
 import { useLayoutEffect } from "react";
-import { Text } from "react-native";
+import { View, StyleSheet } from "react-native";
 
-function ManageExpense({route, navigation}) {
-    const editedExpenseId = route.params?.expenseId;
-    const isEditing = !!editedExpenseId;
+import IconButton from "../components/UI/IconButton";
+import { GlobalStyles } from "../constants/styles";
 
-    useLayoutEffect(() => {
-        navigation.setOptions({
-            title: isEditing ? 'Edit Expense' : 'Add Expense'
-        });
-    }, [navigation, isEditing]);
+function ManageExpense({ route, navigation }) {
+  const editedExpenseId = route.params?.expenseId;
+  const isEditing = !!editedExpenseId;
 
-    
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      title: isEditing ? "Edit Expense" : "Add Expense",
+    });
+  }, [navigation, isEditing]);
 
-    return <Text>Manage Expense Screen</Text>
+  function deleteExpenseHandler() {}
+
+  return (
+    <View style={styles.container}>
+        
+      {isEditing && (
+        <View style={styles.deleteContainer}>
+          <IconButton
+            icon="trash"
+            color={GlobalStyles.colors.error500}
+            size={36}
+            onPress={deleteExpenseHandler}
+          />
+        </View>
+      )}
+    </View>
+  );
 }
 
 export default ManageExpense;
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1, 
+        padding: 24,
+        backgroundColor: GlobalStyles.colors.primary800
+    },
+    deleteContainer: {
+        marginTop: 16,
+        paddingTop: 8,
+        borderTopWidth: 2,
+        borderTopColor: GlobalStyles.colors.primary200,
+        alignItems: 'center'
+    }
+});
