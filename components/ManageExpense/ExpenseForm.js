1ee4aa932f1ff10aa13427537793cce27260fa0c
Uthaeus <romanlavery@gmail.com>
2023-03-19 15:31:54 -0600

    began creating form structure and passing input props to custom input component

--- components/ManageExpense/ExpenseForm.js
+++ components/ManageExpense/ExpenseForm.js
@@ -1,9 +1,26 @@
-import { View, TextInput } from "react-native";
+import { View } from "react-native";
+
+import Input from "./Input";
 
 function ExpenseForm() {
+    function amountChangedHandler() {}
+
     return (
         <View>
-            
+            <Input label='Amount' textInputConfig={{
+                keyboardType: 'decimal-pad',
+                onChangeText: amountChangedHandler,
+            }} />
+            <Input label='Date' textInputConfig={{
+                placeholder: 'YYYY-MM-DD',
+                maxLength: 10,
+                onChangeText: () => {}
+            }} />
+            <Input label='Description' textInputConfig={{
+                multiline: true,
+                autoCapitalize: 'sentences',
+                // autoCorrect: false
+            }} />
         </View>
     );
 }
